3a250814a977b4a2d28ed1107ad40616e469533c
Madhavpokhrel <madhavpokhrel05@gmail.com>
2023-08-29 14:09:27 -0400

    allowed more origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS with appropriate options
   app.enableCors({
-    origin: 'http://localhost:3001', // Replace with your React app's URL
+    origin: ['http://localhost:3001', 'http://localhost:3000'], // Replace with your React app's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
